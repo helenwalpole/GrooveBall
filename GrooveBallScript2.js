43b4d2c533055bc3f505fc1ded84c0652bd5c764
Tom Stringer <t.stringer@unimelb.edu.au>
2021-03-04 20:08:40 +1100

    Make the scores be the resetters

--- GrooveBallScript2.js
+++ GrooveBallScript2.js
@@ -1,4 +1,5 @@
-var list = document.getElementsByClassName("button"), // Fetch all the elements with a class of 'button'
+var buttons = document.getElementsByClassName("button"), // Fetch all the elements with a class of 'button'
+	scores = document.getElementsByClassName("score"), // Fetch all the elements with a class of 'score'
 	player1score = document.getElementById("player1score"), // Get the current player 1 score element
 	player2score = document.getElementById("player2score"); // Get the current player 2 score element
 
@@ -9,8 +10,8 @@ function calculateScores(player, scoreChange){
     player.innerHTML = newScore; // Replace the old score with the new one
 }
 
-for (var i = 0; i < list.length; i++) { // For each element with the button class
-    list[i].addEventListener('click', function() { // Bind a click event that does this stuff
+for (var i = 0; i < buttons.length; i++) { // For each element with the button class
+    buttons[i].addEventListener('click', function() { // Bind a click event that does this stuff
     	event.preventDefault(); // Stop the browser default (so it doesn't jump to the top of the page)
 
     	var player = this.parentNode, // Fetch the clicked element's parent node (helps us figure out which player this is for)
@@ -22,14 +23,17 @@ for (var i = 0; i < list.length; i++) { // For each element with the button clas
     		calculateScores(player1score, scoreChange); // Run the calculateScores function using player 1's deets
     	} else if (player.className === "player2"){ //If the className of the clicked element's parent is player2
     		calculateScores(player2score, scoreChange); // Run the calculateScores function using player 2's deets
-    	} else { // If it's neither of those, it must be the reset button
-    		if (confirm('Are you sure you want to do that?')) { // Show a browser alert asking if you really want to do it
-			  console.log('Game has been reset!'); // If they confirm, reset the scores
-			  document.getElementById("player1score").innerHTML = 0;
-    		  document.getElementById("player2score").innerHTML = 0;
-			} else { // If they cancel, don't do anything
-			  console.log('Phew!');
-			}
     	}
 	});
+}
+
+for (var i = 0; i < scores.length; i++) { // For each element with the button class
+    scores[i].addEventListener('click', function() { // Bind a click event that does this stuff
+    	event.preventDefault(); // Stop the browser default (so it doesn't jump to the top of the page)
+    	if (confirm('Are you sure you want to do that?')) { // Show a browser alert asking if you really want to do it
+			  this.innerHTML = 0;
+		} else { // If they cancel, don't do anything
+			  console.log('Phew!');
+		}
+    });
 }
