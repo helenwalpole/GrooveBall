e7b2c8d6a1a9ac3ee9c3ae57e11b1995b804b18a
Tom Stringer <t.stringer@unimelb.edu.au>
2021-03-05 12:32:47 +1100

    Remove button images

--- GrooveBallScript.js
+++ GrooveBallScript.js
@@ -1,7 +1,22 @@
 var buttons = document.getElementsByClassName("button"), // Fetch all the elements with a class of 'button'
 	scores = document.getElementsByClassName("score"), // Fetch all the elements with a class of 'score'
-	player1score = document.getElementById("player1score"), // Get the current player 1 score element
-	player2score = document.getElementById("player2score"); // Get the current player 2 score element
+	playerOneTotalEl = document.getElementById("player1score"), // Get the current player 1 score element
+	playerTwoTotalEl = document.getElementById("player2score"), // Get the current player 2 score element
+    playerOneScores = [],
+    playerTwoScores = [];
+
+function sum(accumulator, currentValue) {
+    return accumulator + currentValue;
+}
+
+function render() {
+    var playerOneTotal = playerOneScores.reduce(sum, 0)
+    var playerTwoTotal= playerTwoScores.reduce(sum, 0)
+    playerOneTotalEl.innerText = playerOneTotal;
+    playerTwoTotalEl.innerText = playerTwoTotal;
+}
+
+
 
 function calculateScores(player, scoreChange){
 	currentScore = parseInt(player.textContent); // Fetch the text content of the current players score and make it a number
@@ -23,17 +38,13 @@ for (var i = 0; i < buttons.length; i++) { // For each element with the button c
     		calculateScores(player1score, scoreChange); // Run the calculateScores function using player 1's deets
     	} else if (player.className === "player2"){ //If the className of the clicked element's parent is player2
     		calculateScores(player2score, scoreChange); // Run the calculateScores function using player 2's deets
-    	}
+    	} else {
+            if (confirm('Do you want to reset for a new game?')) { // Show a browser alert asking if you really want to do it
+                player1score.innerHTML = 0;
+                player2score.innerHTML = 0;
+            } else { // If they cancel, don't do anything
+                console.log('Phew!');
+            }
+        }
 	});
-}
-
-for (var i = 0; i < scores.length; i++) { // For each element with the button class
-    scores[i].addEventListener('click', function() { // Bind a click event that does this stuff
-    	event.preventDefault(); // Stop the browser default (so it doesn't jump to the top of the page)
-    	if (confirm('Are you sure you want to do that?')) { // Show a browser alert asking if you really want to do it
-			  this.innerHTML = 0;
-		} else { // If they cancel, don't do anything
-			  console.log('Phew!');
-		}
-    });
 }
