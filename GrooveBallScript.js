68d2e0e06655f9f90114f996a7d3f7eba6310e7a
Tom Stringer <t.stringer@unimelb.edu.au>
2021-03-06 14:28:18 +1100

    Tidy up and comment All The Things

--- GrooveBallScript.js
+++ GrooveBallScript.js
@@ -1,3 +1,4 @@
+// Fetch all the things
 var newGameEl = document.getElementsByClassName("intro")
 var newGameButtons = document.getElementsByClassName("newgame")
 var scoreBoardEl = document.getElementsByClassName("scoring")
@@ -14,19 +15,29 @@ var targetScore
 var custom = document.getElementById("custom")
 var customScore = document.getElementById("customScore")
 
-function sum(accumulator, currentValue) {
-    return accumulator + currentValue;
+
+// Function that controls the game (re)starts
+function newGame(action) { // Show the start of a new game or reset everything to start again
+    if (action === 'start') { // If starting a new game
+        newGameEl[0].classList.add("hidden"); // Hide the intro screen
+        scoreBoardEl[0].classList.remove("hidden"); // Show the scoreboard
+        document.body.classList.remove("introLayout"); // Remove the 'introlayout' class from the body tag
+    } else { // If resetting 
+        newGameEl[0].classList.remove("hidden"); // Show the intro screen
+        scoreBoardEl[0].classList.add("hidden"); // Hide the scoreboard
+        document.body.classList.add("introLayout"); // Add the 'introLayout' class to the body tag
+        playerOneTotalEl.innerHTML = 0; // Reset the score element for Player 1
+        playerTwoTotalEl.innerHTML = 0; // Reset the score element for Player 2
+        playerOneScores = []; // Reset the score array for Player 1
+        playerTwoScores = []; // Reset the score array for Player 1
+        custom.classList.add("hidden"); // Hide the 'custom target' div
+        customScore.value = ''; // Reset the custom score value
+    }
 }
 
-function victory(playerImg) {
-    playerImg.classList.remove("hidden");
-    mask.classList.remove("hidden");
-    playerImg.addEventListener('click', function() { // Bind a click event that does this stuff
-        event.preventDefault();
-        playerImg.classList.add("hidden");
-        mask.classList.add("hidden");
-        newGame();
-    });
+// Functions to add up and calculate the scores
+function sum(accumulator, currentValue) {
+    return accumulator + currentValue;
 }
 
 function calculateScores(player, scoreChange, playerEl) {
@@ -36,53 +47,46 @@ function calculateScores(player, scoreChange, playerEl) {
         player.push(scoreChange); // Push the new score to the array
     }
 
-    var newScore = player.reduce(sum, 0);
-    playerEl.innerHTML = newScore; // Update the score
+    var newScore = player.reduce(sum, 0); // Add the values in the player's score array (using the 'sum' function above)
+    playerEl.innerHTML = newScore; // Update the score element
 
-    if (newScore >= targetScore) {
-        if (playerEl.id === 'player1score') {
-            victory(img1);
-        } else {
-            victory(img2);
+    if (newScore >= targetScore) { // If this wins the game
+        if (playerEl.id === 'player1score') { // If the current player is Player 1
+            victory(img1); // Show the Player 1 victory image
+        } else { // If the current player is Player 2
+            victory(img2); // Show the Player 2 victory image
         }
     }
 }
 
-function newGame(action) {
-    if (action === 'start') {
-        newGameEl[0].classList.add("hidden");
-        scoreBoardEl[0].classList.remove("hidden");
-        document.body.classList.remove("introLayout");
-    } else {
-        newGameEl[0].classList.remove("hidden");
-        scoreBoardEl[0].classList.add("hidden");
-        document.body.classList.add("introLayout");
-        playerOneTotalEl.innerHTML = 0;
-        playerTwoTotalEl.innerHTML = 0;
-        playerOneScores = [];
-        playerTwoScores = [];
-        custom.classList.add("hidden");
-        customScore.value = '';
-        console.log(customScore);
-    }
+// Function to show the victory pictures
+function victory(playerImg) {
+    playerImg.classList.remove("hidden"); // Show the player image div contents
+    mask.classList.remove("hidden"); // Show the mask div
+    playerImg.addEventListener('click', function() { // Bind a click event to the mask layer so it can be dismissed
+        event.preventDefault();
+        playerImg.classList.add("hidden"); // Hide the victory image
+        mask.classList.add("hidden"); // Hide the mask
+        newGame(); // Return to the new game screen
+    });
 }
 
+// Settings for the 'new game' buttons
 for (var i = 0; i < newGameButtons.length; i++) { // For each element with the button class
     newGameButtons[i].addEventListener('click', function() { // Bind a click event that does this stuff
         event.preventDefault(); // Stop the browser default (so it doesn't jump to the top of the page)
 
         var classes = this.classList // Get the list of classes used on this element
 
-        if (classes.contains("pick")) {
-            custom.classList.remove("hidden");
-        } else if (classes.contains("custom")) {
-            targetScore = document.getElementById("customScore").value;
-            console.log(targetScore);
-            newGame('start');
-        } else {
-            targetScore = this.getAttribute("data-target");
-            console.log(targetScore);
-            newGame('start');
+        if (classes.contains("pick")) { // If the 'Pick a custom target' button is clicked
+            custom.classList.remove("hidden"); // Show the custom target field
+        } else if (classes.contains("custom")) { // If the 'Let's do eeet' button is clicked
+            targetScore = document.getElementById("customScore").value; // Set the target value to the entered value
+            newGame('start'); // Start the game
+        } else { // If the 'Classic' or 'Test match' buttons are clicked
+            targetScore = this.getAttribute("data-target"); // Set the target value using the data attribute
+            newGame('start'); // Start the game
+
         }
 
     });
